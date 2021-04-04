8f54d32190bfd786cffa1121b592050b70a9b828
WalukaUI <cwaluka@yahoo.com>
2021-04-03 20:27:23 -0700

    Moving all directions

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Your code here
+// var newPosition = dodger.style.left
+// newPosition = parseInt(newPosition)
+
+
+const dodger = document.getElementById('dodger')
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+  });
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+  });
+
+
+  function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+      dodger.style.left = `${left - 5}px`;
+    }else{
+        dodger.style.left = "0px";
+    }
+  }
+
+
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0 && left <=360) {
+      dodger.style.left = `${left + 5}px`;
+    }else{
+        dodger.style.left = "0px";
+    }
+  }
+
+  function moveDodgerUp(){
+    const leftNumbers = dodger.style.bottom.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0 && left <=380) {
+        dodger.style.bottom = `${left + 5}px`;
+    }else{
+        dodger.style.bottom = "0px";
+    }
+  }
+
+  function moveDodgerDown(){
+    const leftNumbers = dodger.style.bottom.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+        dodger.style.bottom = `${left - 5}px`;
+    }
+  }
